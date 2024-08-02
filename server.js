882ded05b9d1e8f8a50bd514fe31fac0d7328190
Hassan Nofisat Adetola <hassannofisat017@gmail.com>
2024-08-02 11:17:39 +0100

    updated

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`The app is running on http://localhost:${PORT}`);
 });
